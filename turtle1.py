7d95c9b669c38f398743b3a1c5d74eaa69f76c6d
VanNguyen1102 <45144146+VanNguyen1102@users.noreply.github.com>
2018-11-25 22:02:00 +0700

    Create turtle1.py

---
+++ turtle1.py
@@ -0,0 +1,24 @@
+from turtle import *
+
+color ("red")
+
+right(30)
+forward(50)
+left(60)
+forward(50)
+left(120)
+forward(50)
+left(60)
+forward(50)
+
+for i in range (3):
+    right(150)
+    forward(50)
+    left(60)
+    forward(50)
+    left(120)
+    forward(50)
+    left(60)
+    forward(50)
+
+mainloop()
